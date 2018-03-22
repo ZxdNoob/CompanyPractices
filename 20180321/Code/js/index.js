e2f121bd6b0dc084cf869c3c67b11d0db6015d81
ZxdNoob <1023407467@qq.com>
2018-03-22 18:59:24 +0800

    verson 1.1

--- 20180321/Code/js/index.js
+++ 20180321/Code/js/index.js
@@ -1,7 +1,18 @@
-//顶部导航下拉菜单的显示与隐藏控制
+//搜索框搜索对象转换
 var getEleById = function(id) {
     return document.getElementById(id);
 };
-var tbList = getEleById("tb");
-console.log(tbList);
-var subShow = function() {}
+
+var topInit = getEleById("goods"); //顶部已选项初始值
+var botInit = getEleById("shop"); //底部可选项初始值
+var selectList = getEleById("selected");
+var typeArr = ["宝贝", "店铺"]; //类型数组初始值
+
+var changeType = function() {
+    //让顶部可选项永远保持是类型数组的第一项
+    topInit.innerHTML = typeArr.reverse()[0];
+    //让顶部可选项永远保持是类型数组的第二项
+    botInit.innerHTML = typeArr[1];
+};
+
+selectList.onclick = changeType;
