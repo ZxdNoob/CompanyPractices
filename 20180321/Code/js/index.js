b1a78508072af8340e1375995c7dbdb67df7c342
ZxdNoob <1023407467@qq.com>
2018-03-23 16:40:21 +0800

    version 1.2

--- 20180321/Code/js/index.js
+++ 20180321/Code/js/index.js
@@ -1,11 +1,50 @@
-//搜索框搜索对象转换
-var getEleById = function(id) {
-    return document.getElementById(id);
+//用对象字面量来封装获取标签的相关方法
+var $ = {
+    name: "获取元素的方法",
+    //通过id值获取标签
+    byId: function(id) {
+        return document.getElementById(id);
+    },
+
+    //通过class获取标签
+    byClass: function(className) {
+        return document.getElementsByClassName(className);
+    },
+
+    //通过标签名获取标签
+    byTag: function(tagName) {
+        return document.getElementsByTagName(tagName);
+    }
 };
 
-var topInit = getEleById("goods"); //顶部已选项初始值
-var botInit = getEleById("shop"); //底部可选项初始值
-var selectList = getEleById("selected");
+//动态给网址导航栏各分栏列表项分配宽度
+var aListWrapper = $.byClass("site-list");
+for (var i = 0, len = aListWrapper.length; i < len; i++) {
+    var aSiteList = aListWrapper[i].children[1]; //获取无序列表
+    var liLen = aSiteList.children.length; //存储无序列表的列表项数目
+    if (liLen > 27) { //列表项的数目大于27
+        var liL = aSiteList.children;
+        for (var j = 0, mosLen = liL.length; j < mosLen; j++) {
+            liL[j].style.width = "24%";
+        }
+    } else if (liLen <= 27 && liLen > 12) { //列表项的数目大于12且小于或等于27
+        var liM = aSiteList.children;
+        for (var k = 0, morLen = liM.length; k < morLen; k++) {
+            liM[k].style.width = "33%";
+        }
+    } else { //列表项的数目小于或等于12
+        var liS = aSiteList.children;
+        for (var m = 0, sLen = liS.length; m < sLen; m++) {
+            liS[m].style.width = "49%";
+        }
+    }
+}
+
+//搜索框搜索对象转换
+
+var topInit = $.byId("goods"); //顶部已选项初始值
+var botInit = $.byId("shop"); //底部可选项初始值
+var selectList = $.byId("selected");
 var typeArr = ["宝贝", "店铺"]; //类型数组初始值
 
 var changeType = function() {
